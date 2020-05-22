de16c31b82d0d08e86e94f49f93d7a1b39086bca
igor.melo <igorjmelo4@gmail.com>
2020-05-21 22:02:22 -0300

    ADD: Configuring routes

---
+++ src/pages/Dashboard/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import Background from '~/components/Background';
+
+// import { Container } from './styles';
+
+const Dashboard = () => {
+  return <Background />;
+};
+
+export default Dashboard;
+
+Dashboard.navigationOptions = {
+  tabBarLabel: 'Agendamentos',
+  tabBarIcon: ({ tintColor }) => (
+    <Icon name="event" size={20} color={tintColor} />
+  ),
+};
