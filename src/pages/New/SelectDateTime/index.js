1d1942affe0b4c3ef11cc938b2b3a437226449e4
igor.melo <igorjmelo4@gmail.com>
2020-05-28 21:24:39 -0300

    [+]: Provider List

---
+++ src/pages/New/SelectDateTime/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import Background from '~/components/Background';
+
+// import { Container } from './styles';
+
+const SelectDateTime = () => {
+  return <Background />;
+};
+
+export default SelectDateTime;
+
+SelectDateTime.navigationOptions = {
+  title: 'Selecione o horário',
+};
